3b2bd645e78a62b02c8267e5f7f0e5dc2131b1b7
Pulkit-jhamb <pulkitjhamb005@gmail.com>
2025-10-24 05:27:56 +0530

    chore: add comment about API base URL change in config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // API Configuration
 const API_BASE_URL = 'https://carevo-backend.onrender.com';
+//i have changed the base url to carevo-backend onrender.com
 
-//
 //const API_BASE_URL = 'http://localhost:5001';
 // const API_BASE_URL = 'https://carevo-backend-8vv2.onrender.com/';
 
